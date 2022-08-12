80d1d9f918ad000ab3a1ade28315024ed38b8ffb
Ankit Patel <76809891+ankittops301@users.noreply.github.com>
2022-08-12 14:19:03 +0530

    passing model

--- FirstApp/FirstApp/Controllers/FirstController.cs
+++ FirstApp/FirstApp/Controllers/FirstController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FirstApp.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,15 @@ namespace FirstApp.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult EmpInfo()
+        {
+            List<Employee> li = new List<Employee>();
+            li.Add(new Employee { empid=1,fname="Nikhilesh",mobile="9876", email="n@gmail.com" });
+            li.Add(new Employee { empid=2,fname="Raj",mobile="69281", email="r@gmail.com" });
+            li.Add(new Employee { empid=3,fname="Jaydeep",mobile="72364", email="j@gmail.com" });
+            return View(li);
         }
     }
 }
---
+++ ReturnTypeOfAction/ReturnTypeOfAction/Controllers/DefaultController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReturnTypeOfAction.Controllers
+{
+    public class DefaultController : Controller
+    {
+        private readonly IWebHostEnvironment _env;
+        public DefaultController(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+
+        public ContentResult Index()
+        {
+            return Content(_env.WebRootPath);
+            //return Content("Learning .Net Core in TOPS.");
+            //return Content("<div>Learning <b>.Net Core</b> in TOPS.</div>","text/html");
+        }
+    }
+}
