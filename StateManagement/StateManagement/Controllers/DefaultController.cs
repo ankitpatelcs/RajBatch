d82a6f8b61d6412b74a697c377f58d3844cee47a
Ankit Patel <ankitpatel.tops@gmail.com>
2022-09-02 14:49:47 +0530

    cookies

---
+++ StateManagement/StateManagement/Controllers/DefaultController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StateManagement.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IHttpContextAccessor _contextAccessor;
+        public DefaultController(IHttpContextAccessor contextAccessor)
+        {
+            _contextAccessor = contextAccessor;
+        }
+        public IActionResult Cookies()
+        {
+            CookieOptions options = new CookieOptions();
+            options.Expires = DateTime.Now.AddDays(2);
+            _contextAccessor.HttpContext.Response.Cookies.Append("username", "Raj",options);
+            return View();
+        }
+        public IActionResult Index()
+        {
+            ViewBag.data = _contextAccessor.HttpContext.Request.Cookies["username"].ToString();
+            return View();
+        }
+    }
+}
