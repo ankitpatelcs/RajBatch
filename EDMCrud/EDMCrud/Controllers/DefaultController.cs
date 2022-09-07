a47bc9b7917f8b0ce592cd6e48fd1945379f8d26
Ankit <ankitpatel.tops@gmail.com>
2022-09-07 14:47:16 +0530

    edm

---
+++ EDMCrud/EDMCrud/Controllers/DefaultController.cs
@@ -0,0 +1,23 @@
+﻿using EDMCrud.EDM;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EDMCrud.Controllers
+{
+    public class DefaultController : Controller
+    {
+        private CompanyContext _context = null;
+        public DefaultController(CompanyContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_context.Tblemployees.ToList());
+        }
+    }
+}
