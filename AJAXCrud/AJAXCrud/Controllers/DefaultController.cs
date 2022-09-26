bacd4c6a6357e7f78c100854972702921d25e5b2
Ankit <ankitpatel.tops@gmail.com>
2022-09-26 14:39:02 +0530

    ajax get

---
+++ AJAXCrud/AJAXCrud/Controllers/DefaultController.cs
@@ -0,0 +1,27 @@
+﻿using AJAXCrud.EDM;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AJAXCrud.Controllers
+{
+    public class DefaultController : Controller
+    {
+        private CompanyContext dc = null;
+        public DefaultController(CompanyContext _dc)
+        {
+            dc = _dc;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetEmployees()
+        {
+            return Json(dc.Tblemployees.ToList());
+        }
+    }
+}
---
+++ AJAXCrud/AJAXCrud/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AJAXCrud.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
