1351397cfbbf637bdbac20c885fe515ce7a24443
Ankit <ankitpatel.tops@gmail.com>
2022-09-28 14:37:04 +0530

    config

--- AJAXCrud/AJAXCrud/Controllers/DefaultController.cs
+++ AJAXCrud/AJAXCrud/Controllers/DefaultController.cs
@@ -23,5 +23,41 @@ namespace AJAXCrud.Controllers
         {
             return Json(dc.Tblemployees.ToList());
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public string Create(Tblemployee obj)
+        {
+            dc.Tblemployees.Add(obj);
+            dc.SaveChanges();
+            return "Record Inserted.";
+        }
+        
+        public IActionResult Edit(int id)
+        {
+            return View(dc.Tblemployees.Find(id));
+        }
+        [HttpPost]
+        public string Edit(Tblemployee obj)
+        {
+            dc.Tblemployees.Update(obj);
+            dc.SaveChanges();
+            return "Record Updated.";
+        }
+        
+        public IActionResult Delete(int id)
+        {
+            return View(dc.Tblemployees.Find(id));
+        }
+        [HttpPost,ActionName("Delete")]
+        public string DeleteRec(int id)
+        {
+            dc.Tblemployees.Remove(dc.Tblemployees.Find(id));
+            dc.SaveChanges();
+            return "Record Deleted.";
+        }
     }
 }
---
+++ ConfigFile/ConfigFile/Controllers/DefaultController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConfigFile.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IConfiguration Configuration { get; }
+        public DefaultController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        
+        public IActionResult Index()
+        {
+            ViewBag.SMTPServer = Configuration["MyApplicationKeys:SMTPServer"];
+            ViewBag.AzureSQLServerURL = Configuration["MyApplicationKeys:AzureSQLServerURL"];
+            ViewBag.EmployeePhotoUploadPath = Configuration["MyApplicationKeys:EmployeePhotoUploadPath"];
+            return View();
+        }
+    }
+}
