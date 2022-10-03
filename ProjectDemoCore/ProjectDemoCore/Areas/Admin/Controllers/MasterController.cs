b0da56c9be6b82aae227843654e346d8233ec7ef
Ankit <ankitpatel.tops@gmail.com>
2022-10-03 15:03:42 +0530

    login

---
+++ ProjectDemoCore/ProjectDemoCore/Areas/Admin/Controllers/MasterController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProjectDemoCore.EDM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectDemoCore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MasterController : Controller
+    {
+        private ecommerceContext dc = null;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public MasterController(ecommerceContext _dc, IHttpContextAccessor httpContextAccessor)
+        {
+            dc = _dc;
+            _httpContextAccessor = httpContextAccessor;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(IFormCollection fc)
+        {
+            string email = fc["email"];
+            string pass = fc["password"];
+
+            var data = dc.TblAdmins.Where(x => x.Email == email && x.Password == pass).FirstOrDefault();
+            if (data != null)
+            {
+                _httpContextAccessor.HttpContext.Session.SetString("AdminId", data.AdminId.ToString());
+                _httpContextAccessor.HttpContext.Session.SetString("AdminName", data.FName);
+                return RedirectToAction("Dashboard");
+            }
+            ViewBag.loginerror = "Invalid email or password";
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            _httpContextAccessor.HttpContext.Session.Clear();
+            return RedirectToAction("Login");
+        }
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+        
+        public IActionResult AddProductc()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddProductc(Tblproduct obj)
+        {
+            dc.Tblproducts.Add(obj);
+            dc.SaveChanges();
+            return RedirectToAction("ManageProducts");
+        }
+        public IActionResult ManageProducts()
+        {
+            return View(dc.Tblproducts.ToList());
+        }
+    }
+}
---
+++ ProjectDemoCore/ProjectDemoCore/Areas/Users/Controllers/DefaultController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProjectDemoCore.EDM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectDemoCore.Areas.Users.Controllers
+{
+    [Area("Users")]
+    public class DefaultController : Controller
+    {
+        private ecommerceContext dc = null;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public DefaultController(ecommerceContext _dc, IHttpContextAccessor httpContextAccessor)
+        {
+            dc = _dc;
+            _httpContextAccessor = httpContextAccessor;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(IFormCollection fc)
+        {
+            string email = fc["email"];
+            string pass = fc["password"];
+
+            var data = dc.TblUsers.Where(x => x.Email == email && x.Password == pass).FirstOrDefault();
+            if (data != null)
+            {
+                _httpContextAccessor.HttpContext.Session.SetString("UserId", data.UserId.ToString());
+                _httpContextAccessor.HttpContext.Session.SetString("UserName", data.FName);
+                return RedirectToAction("HomePage");
+            }
+            ViewBag.loginerror = "Invalid email or password";
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            _httpContextAccessor.HttpContext.Session.Remove("UserId");
+            _httpContextAccessor.HttpContext.Session.Remove("UserName");
+            _httpContextAccessor.HttpContext.Session.Clear();
+            return RedirectToAction("HomePage");
+        }
+        public IActionResult HomePage()
+        {
+            return View();
+        }
+    }
+}
